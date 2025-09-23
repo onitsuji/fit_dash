80442ea48257f47983ed5b0d7b4b644102ab31e9
onitsuji <itsjustinoconnell@gmail.com>
2025-09-23 13:18:45 +0100

    landing, auth routes added

--- app/routes.ts
+++ app/routes.ts
@@ -7,8 +7,8 @@ import {
 
 export default [
   index("./routes/home.tsx"),
-  route("signup", "./routes/sign-up.tsx"),
-  route("signin", "./routes/sign-in.tsx"),
+  route("sign-up", "./routes/sign-up.tsx"),
+  route("sign-in", "./routes/sign-in.tsx"),
   layout("./layout/dashboard-layout.tsx", [
     route("dashboard", "./routes/dashboard/dashboard.tsx"),
   ]),
--- app/views/marketing.tsx
+++ app/views/marketing.tsx
@@ -1,25 +1,41 @@
+import { Link } from "react-router";
+import { Button } from "~/components/ui/button";
+
 export function Marketing() {
   return (
-    <div className="bg-blue-500 min-h-svh">
-      <div className="bg-amber-200 max-w-6xl mx-auto">
+    <div className="min-h-svh">
+      <div className="max-w-6xl mx-auto">
         <header>
-          <nav>
-            <div>logo fitdash</div>
+          <nav className="flex items-center justify-between pt-2.5">
+            <div className="flex items-center gap-x-3">
+              <img src="/logo.svg" alt="logo" className="size-9" />{" "}
+              <span>Fitdash</span>
+            </div>
             <div>
-              <div>log in</div>
+              <div>
+                <Link to="/sign-in">
+                  <Button>Log In</Button>
+                </Link>
+              </div>
             </div>
           </nav>
         </header>
-        <section>
-          <div>
-            <h1>Take control of your health, form lasting healthy habits</h1>
-            <p>
+        <section className="mt-14">
+          <div className="space-y-2.5">
+            <h1 className="text-4xl">
+              Take control of your health, form lasting healthy habits
+            </h1>
+            <p className="text-balance">
               Build a healthier lifestyle with small, consistent steps. Our
               tools guide you to set clear goals, track progress, and stay
               motivated—so you can create habits that last and take charge of
               your well-being.
             </p>
-            <div>Get started for free</div>
+            <div>
+              <Link to="/sign-up">
+                <Button variant="secondary">Get started</Button>
+              </Link>
+            </div>
           </div>
         </section>
       </div>
