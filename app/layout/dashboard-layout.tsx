02e87dbf390cd622cfc5a8243fa9a79e47d0b927
onitsuji <itsjustinoconnell@gmail.com>
2025-09-24 15:41:53 +0100

    dashboard sidebar

--- app/layout/dashboard-layout.tsx
+++ app/layout/dashboard-layout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router";
+import { AppSidebar } from "~/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
+import { type VariantProps, cva } from "class-variance-authority";
 
 export default function DashboardLayout() {
   return (
-    <div className="flex flex-row p-5">
-      <div className="w-12">sidebar</div>
-      <div className="ml-2">
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
         <Outlet />
-      </div>
-    </div>
+      </main>
+    </SidebarProvider>
   );
 }
---
+++ todo.md
@@ -0,0 +1,9 @@
+## Auth
+
+### Sign-Up
+- Constraints to schema w/ messages
+- Error Alert for Better Auth api errors in form
+- Add fake t&c's in card footer
+
+### Sign-In
+- Clone sign-up view and alter schema
